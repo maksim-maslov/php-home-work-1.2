05a2845c7cf07defb34d694012fa5394a27f4c79
maksim-maslov <brat.maksim@gmail.com>
2017-07-20 00:05:40 +0300

    добавлен код реализации алгоритма

--- homework-1.2.php
+++ homework-1.2.php
@@ -1,3 +1,21 @@
 <?php
+$x = rand(0, 100);
+echo "Число ".$x;
 
+$variable1 = 1;
+$variable2 = 2;
+
+while (1) {
+	if ($variable1 > $x) {
+		echo " задуманное число НЕ входит в числовой ряд";
+		break;
+	} elseif ($variable1 = $x) {
+		echo " задуманное число входит в числовой ряд";
+		break;
+	} else {
+		$variable3 = $variable1;
+		$variable1 +=$variable2;
+		$variable2 = $variable3;
+	}
+}
 ?>
